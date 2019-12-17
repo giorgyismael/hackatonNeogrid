a61320bd8fb5b976a4f563f454495cd362c0890d
GaryLima <garylimalage@gmail.com>
2019-12-17 18:17:36 -0300

    Implementação com movefile

--- src/main/java/application/FileLoader.java
+++ src/main/java/application/FileLoader.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 import vo.ProductVO;
 import vo.VehicleVO;
 
-public class FileLoader {
+public class FileLoader{
 
 	public String buildFile(File file, ArrayList<ProductVO> listProduct, ArrayList<VehicleVO> listVehicle) {
 
@@ -17,8 +20,6 @@ public class FileLoader {
 
 		entityFilter(listFiles, listProduct, listVehicle);
 
-		System.out.println(listVehicle.toString());
-		System.out.println(listProduct.toString());
 		return String.valueOf(file);
 	}
 
@@ -30,19 +31,36 @@ public class FileLoader {
 				String line = br.readLine();
 
 				String type = line;
-				String weight = br.readLine();
+				Double weight = Double.valueOf(br.readLine());
 				String destiny = br.readLine();
 
-				if (path.toString().contains("caminhao")) {
-					vehicle = VehicleVO.builder().type(type).destiny(destiny).amount(Double.valueOf(weight)).build();
+				if (path.toString().contains("caminhao") && weight > 0) {
+					vehicle = VehicleVO.builder().type(type).destiny(destiny).amount(weight).build();
 					listVehicle.add(vehicle);
-				} else {
-					product = ProductVO.builder().type(type).destiny(destiny).amount(Double.valueOf(weight)).build();
+
+					moveFile(path);
+
+				} else if (weight > 0) {
+					product = ProductVO.builder().type(type).destiny(destiny).amount(weight).build();
 					listProduct.add(product);
+
+					moveFile(path);
 				}
 			} catch (IOException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
+
+			System.out.println(listVehicle.toString());
+			System.out.println(listProduct.toString());
+		}
+	}
+
+	private void moveFile(File path) {
+		if (path.renameTo(new File("C:\\Users\\User-000\\Desktop\\Dados Hackaton 2019\\teste1\\" + path.getName()))) {
+			System.out.println("File is moved successful!");
+			path.delete();
+		} else {
+			System.out.println("File is failed to move!");
 		}
 	}
 }
